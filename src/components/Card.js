9bb75d790f0c571b696454a912bf715049d305ea
Marc Collado <marc@collado.io>
2018-06-15 23:56:52 +0200

    Add image background to Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, } from 'react-native';
+import { View, Text, Image, } from 'react-native';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
@@ -8,6 +8,9 @@ import { white, black, } from '../utils/ui/colors';
 
 const Card = ({ children, style, }) => (
   <View style={style}>
+    <StyledImage
+      source={{uri: 'https://images.unsplash.com/photo-1464490997959-0c65eee1cc26?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a94e63e6f9e5de6163c5343adfe75689&auto=format&fit=crop&w=1350&q=80'}}
+    />
     <LargeTitle>
       {children}
     </LargeTitle>
@@ -19,12 +22,18 @@ const Card = ({ children, style, }) => (
 
 const StyledCard = styled(Card)`
   width: 80%;
-  height: 300;
+  height: 340;
   border-radius: 8px;
   background-color: ${white};
-  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.08);
+  box-shadow: 0px 4px 8px rgba(25, 25, 25, 0.15);
 `;
 
+const StyledImage = styled(Image)`
+  width: 100%;
+  height: 60%;
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+`
 
 Card.propTypes = {
 

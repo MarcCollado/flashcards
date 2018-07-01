edd6bd3e0098943651645815741d752f1a4ffa9d
Marc Collado <marc@collado.io>
2018-07-01 07:38:14 +0200

    Update .env vars for Unsplash API

--- src/utils/api/vars.js
+++ src/utils/api/vars.js
@@ -1,3 +1,17 @@
+import Unsplash from 'unsplash-js/native';
+import {
+  UNSPLASH_APP_ACCESS_KEY,
+  UNSPLASH_APP_SECRET,
+  UNSPLASH_CALLBACK_URL,
+} from 'react-native-dotenv';
+
+// unsplash app auth
+export const unsplash = new Unsplash({
+  applicationId: UNSPLASH_APP_ACCESS_KEY,
+  secret: UNSPLASH_APP_SECRET,
+  callbackUrl: UNSPLASH_CALLBACK_URL,
+});
+
 // AsyncStorage key
 export const STORAGE_KEY = `flashcards`;
 // URL endpoint for GraphQL API calls
